046ea3c41e8948f831cf13b144376b395d93fdcd
joy <vxer@qq.com>
2023-04-21 18:49:00 +0800

    pipeline_state

---
+++ source/runtime/core/pipeline_state.cpp
@@ -0,0 +1,27 @@
+#include "pipeline_state.h"
+
+pso::pso(wchar_t const* name)
+	: name_(name),
+	root_signature_(nullptr),
+	rhi_pso_(nullptr)
+{
+}
+
+pso::~pso()
+{
+}
+
+void pso::set_root_signature(root_signature const* signature)
+{
+	root_signature_ = signature;
+}
+
+root_signature const* pso::get_root_signature() const
+{
+	return root_signature_;
+}
+
+ID3D12PipelineState* pso::get_pipeline_state_object() const
+{
+	return rhi_pso_;
+}
