59f05fb5ac1b4bc57446400ed2f52b23fbf6e95b
joy <vxer@qq.com>
2023-04-24 13:30:00 +0800

    gpu_buffer

--- source/runtime/core/gpu_buffer.cpp
+++ source/runtime/core/gpu_buffer.cpp
@@ -1,1 +1,74 @@
 #include "gpu_buffer.h"
+#include "graphics_core.h"
+
+D3D12_RESOURCE_DESC gpu_buffer::describe_buffer() const
+{
+    D3D12_RESOURCE_DESC resource_desc = {};
+    resource_desc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+    resource_desc.Alignment = 0;
+    resource_desc.Width = buffer_size_;
+    resource_desc.Height = 1;
+    resource_desc.DepthOrArraySize = 1;
+    resource_desc.MipLevels = 1;
+    resource_desc.Format = DXGI_FORMAT_UNKNOWN;
+    resource_desc.SampleDesc.Count = 1;
+    resource_desc.SampleDesc.Quality = 0;
+    resource_desc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+    resource_desc.Flags = resource_flags_;
+    return resource_desc;
+}
+
+void gpu_buffer::create_buffer_and_view(wchar_t const* name, uint32_t num_elements, uint32_t element_size)
+{
+    destroy();
+
+    num_elements_ = num_elements;
+    element_size_ = element_size;
+    buffer_size_ = num_elements * element_size;
+
+    D3D12_RESOURCE_DESC resource_desc = describe_buffer();
+
+    D3D12_HEAP_PROPERTIES heap_prop = {};
+    heap_prop.Type = D3D12_HEAP_TYPE_DEFAULT;
+    heap_prop.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+    heap_prop.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+    heap_prop.CreationNodeMask = 0;
+    heap_prop.VisibleNodeMask = 0;
+
+    usage_state_ = D3D12_RESOURCE_STATE_COMMON;
+
+    ASSERT_SUCCEEDED(get_rhi()->device->CreateCommittedResource(&heap_prop, D3D12_HEAP_FLAG_NONE, &resource_desc, usage_state_, nullptr, IID_PPV_ARGS(&rhi_resource_)));
+    rhi_resource_->SetName(name);
+
+    gpu_virtual_address_ = rhi_resource_->GetGPUVirtualAddress();
+
+    create_derived_views();
+}
+
+int gpu_buffer::create(wchar_t const* name, uint32_t num_elements, uint32_t element_size, const void* initial_data)
+{
+    create_buffer_and_view(name, num_elements, element_size);
+
+    if (initial_data)
+    {
+        assert(false && "not impl yet");
+    }
+
+    return 0;
+}
+
+int gpu_buffer::create(wchar_t const* name, uint32_t num_elements, uint32_t element_size, const upload_buffer& src_data, uint32_t src_offset)
+{
+    create_buffer_and_view(name, num_elements, element_size);
+
+    // TODO: copy init data
+    assert(false && "not impl yet");
+
+    return 0;
+}
+
+int gpu_buffer::create(wchar_t const* name, ID3D12Heap* backing_heap, uint32_t heap_offset, uint32_t num_elements, uint32_t element_size, const void* initial_data)
+{
+    assert(false && "not impl yet");
+    return -1;
+}
--- source/runtime/core/gpu_buffer.h
+++ source/runtime/core/gpu_buffer.h
@@ -2,7 +2,34 @@
 
 #include "gpu_resource.h"
 
+class upload_buffer;
+
+/*
+===============================================================================
+≥ÈœÛ¿‡£ª
+===============================================================================
+*/
 class gpu_buffer : public gpu_resource
 {
-	//TODO:
+public:
+    // Create a buffer.  If initial data is provided, it will be copied into the buffer using the default command context.
+    int create(wchar_t const* name, uint32_t num_elements, uint32_t element_size, const void* initial_data = nullptr);
+    int create(wchar_t const* name, uint32_t num_elements, uint32_t element_size, const upload_buffer& src_data, uint32_t src_offset = 0);
+
+    // Sub-Allocate a buffer out of a pre-allocated heap.  If initial data is provided, it will be copied into the buffer using the default command context.
+    int create(wchar_t const* name, ID3D12Heap* backing_heap, uint32_t heap_offset, uint32_t num_elements, uint32_t element_size, const void* initial_data = nullptr);
+
+protected:
+    D3D12_RESOURCE_DESC describe_buffer() const;
+
+    virtual void create_derived_views(void) = 0;
+
+private:
+    void create_buffer_and_view(wchar_t const* name, uint32_t num_elements, uint32_t element_size);
+
+protected:
+    uint32_t num_elements_;
+    uint32_t element_size_;
+    size_t buffer_size_;
+    D3D12_RESOURCE_FLAGS resource_flags_;
 };
