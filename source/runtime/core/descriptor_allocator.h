93dbcaa4b0c9ccc0a1ce28351fb3088f7a32243e
joy <vxer@qq.com>
2023-04-26 10:41:13 +0800

    Doxygen配置

--- source/runtime/core/descriptor_allocator.h
+++ source/runtime/core/descriptor_allocator.h
@@ -4,37 +4,49 @@
 #include "utility.h"
 #include <mutex>
 
-/*
-===============================================================================
-Heap可以理解为一个数组，Heap的类型决定了每个元素的大小，另外还要指定数组的长度；
-Hanlde可以理解为数组中某个元素的指针；
-Handle分CPU和GPU，它们的地址是不一样的？
-
-FIXME: 几个static成员是否可以改成非static的？
-
-@see descriptor_heap
-===============================================================================
-*/
+/**
+ * @brief 
+ * 与descriptor_heap相比，本类的Heap数量是可以动态增长的。当前Heap剩余空间不足时，
+ * 会再分配一个Heap。
+ * <pre>
+ * 关于Heap的说明：
+ * Heap可以理解为一个数组，Heap的类型决定了每个元素的大小，另外还要指定数组的长度。
+ * Hanlde可以理解为数组中某个元素的指针。
+ * Handle分CPU和GPU，它们的地址是不一样的。
+ * </pre>
+ *
+ * FIXME: 几个static成员是否可以改成非static的？
+ * 
+ * @see descriptor_heap
+ */
 class descriptor_allocator
 {
 public:
 	descriptor_allocator(D3D12_DESCRIPTOR_HEAP_TYPE heap_type);
 
+	/** 
+	 * @brief 从堆中分配指定数量的描述符空间
+	 * @param count 描述符数量
+	 * @return 起始句柄
+	 */
 	D3D12_CPU_DESCRIPTOR_HANDLE allocate(uint32_t count);
+
+	/** @brief 删除所有创建的堆 */
 	static void destroy_all();
 
 private:
+	/** @brief 创建一个新堆 */
 	static ID3D12DescriptorHeap* request_new_heap(D3D12_DESCRIPTOR_HEAP_TYPE heap_type);
 
 	descriptor_allocator(descriptor_allocator const&) = delete;
 
 private:
-	static std::mutex mutex_;
-	static std::vector<ComPtr<ID3D12DescriptorHeap>> heap_pool_;
+	static std::mutex mutex_; ///< 锁
+	static std::vector<ComPtr<ID3D12DescriptorHeap>> heap_pool_; ///< 已分配的堆
 
-	D3D12_DESCRIPTOR_HEAP_TYPE heap_type_;
-	ID3D12DescriptorHeap* current_heap_;
-	D3D12_CPU_DESCRIPTOR_HANDLE current_handle_;
-	uint32_t free_handles_;
-	uint32_t descriptor_size_;
+	D3D12_DESCRIPTOR_HEAP_TYPE heap_type_; ///< 堆类型
+	ID3D12DescriptorHeap* current_heap_; ///< 最近分配的堆
+	D3D12_CPU_DESCRIPTOR_HANDLE current_handle_; ///< 最近堆中的下次可分配地址
+	uint32_t free_handles_; ///< 最近堆中的可分配数量
+	uint32_t descriptor_size_; ///< 堆中一个元素的大小
 };
--- source/runtime/core/descriptor_heap.h
+++ source/runtime/core/descriptor_heap.h
@@ -32,10 +32,10 @@ private:
 };
 
 /**
- * @brief Heap是一个数组，因此需要指定元素类型(描述符堆类型)、数组长度；
- * HANDLE就是一个指针；
- * 有了堆的起始内存地址，可以求出任一元素的地址；
- * 描述符堆类型一共有4种(各种View以及Sampler)；
+ * @brief 
+ * 本来管理的Heap的大小是固定的，无法动态增长。
+ * Heap是一个数组，因此需要指定元素类型(描述符堆类型)、数组长度。HANDLE就是一个指针。
+ * 有了堆的起始内存地址，根据元素大小，可以求出任一元素的地址。描述符堆类型一共有4种(各种View以及Sampler)。
  * 
  * @see descriptor_allocator
  */
@@ -48,30 +48,38 @@ public:
 	void create(wchar_t const* debug_heap_name, D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t max_count);
 	void destroy();
 
-	//是否有可用数量的描述符
+	/** @brief 是否有可用数量的描述符 */
 	bool has_available_space(uint32_t count);
-	//分配指定数量的描述符
+	/** @brief 分配指定数量的描述符 */
 	descriptor_handle alloc(uint32_t count);
 
-	//获取指定索引的元素的句柄
+	/** 
+	 * @brief 获取指定索引的元素的句柄
+	 * @param index 元素索引
+	 * @return 第index个数组元素的句柄
+	 */
 	descriptor_handle operator[](uint32_t index) const;
 
-	//这个handle是第几个元素
+	/** 
+	 * @brief 这个handle是第几个元素
+	 * @return 句柄对应的元素索引
+	 */
 	uint32_t get_offset_of_handle(const descriptor_handle& handle);
-	//是否是有效的句柄(指针是否越界)
-	bool is_valid_handle(const descriptor_handle& handle) const;
-	//堆中每个元素的大小
-	uint32_t get_descriptor_size() const;
 
+	/** @brief 是否是有效的句柄(指针是否越界) */
+	bool is_valid_handle(const descriptor_handle& handle) const;
+	/** @brief 堆中每个元素的大小 */
+	uint32_t get_descriptor_size() const;
+	/** @brief 获取堆的接口指针 */
 	ID3D12DescriptorHeap* get_heap_pointer() const;
 
 private:
 	ComPtr<ID3D12DescriptorHeap> rhi_heap_;
 	D3D12_DESCRIPTOR_HEAP_DESC heap_desc_;
-	uint32_t descriptor_size_; //元素大小
-	uint32_t free_descriptors_num_; //未使用的描述符个数
-	descriptor_handle first_handle_; //堆的起始地址
-	descriptor_handle next_free_handle_; //可分配的描述符句柄
+	uint32_t descriptor_size_; ///< 元素大小
+	uint32_t free_descriptors_num_; ///< 未使用的描述符个数
+	descriptor_handle first_handle_; ///< 堆的起始地址
+	descriptor_handle next_free_handle_; ///< 可分配的描述符句柄
 };
 
 #include "descriptor_heap.inl"
