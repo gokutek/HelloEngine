73fdfd750f46a4975d631588ca74f6b1632b29a4
joy <vxer@qq.com>
2023-05-06 17:36:51 +0800

    allocator

--- source/runtime/core/linear_allocation_page.cpp
+++ source/runtime/core/linear_allocation_page.cpp
@@ -1,6 +1,31 @@
 ﻿#include "linear_allocation_page.h"
 
+linear_allocation_page::linear_allocation_page(ID3D12Resource* rhi_resource, D3D12_RESOURCE_STATES initial_resource_state)
+{
+	rhi_resource_.Attach(rhi_resource);
+	usage_state_ = initial_resource_state;
+	gpu_virtual_address_ = rhi_resource_->GetGPUVirtualAddress();
+	rhi_resource_->Map(0, nullptr, &cpu_virtual_address_);
+}
+
+linear_allocation_page::~linear_allocation_page()
+{
+	unmap();
+}
+
+void linear_allocation_page::map()
+{
+	if (!cpu_virtual_address_)
+	{
+		rhi_resource_->Map(0, nullptr, &cpu_virtual_address_);
+	}
+}
+
 void linear_allocation_page::unmap()
 {
-
+	if (cpu_virtual_address_)
+	{
+		rhi_resource_->Unmap(0, nullptr);
+		cpu_virtual_address_ = nullptr;
+	}
 }
--- source/runtime/core/linear_allocation_page.h
+++ source/runtime/core/linear_allocation_page.h
@@ -3,16 +3,34 @@
 #include "gpu_resource.h"
 
 /**
- * @brief 表示GPU上的一块内存页
- * 
- * TODO:
+ * @brief 抽象表示一块BUFF内存资源
  */
 class linear_allocation_page : public gpu_resource
 {
 public:
-    linear_allocation_page(ID3D12Resource* pResource, D3D12_RESOURCE_STATES Usage);
+    /**
+     * @brief 构造函数
+     * @param rhi_resource 由外部创建的资源，所有权将转移到本对象
+     * @param initial_resource_state 初始状态
+     */
+    linear_allocation_page(ID3D12Resource* rhi_resource, D3D12_RESOURCE_STATES initial_resource_state);
 
+    /**
+     * @brief 析构时会自动unmap
+     */
+    ~linear_allocation_page();
+
+    /**
+     * @brief 映射到CPU
+     */
+    void map();
+
+    /**
+     * @brief 与map对应
+     */
     void unmap();
 
 public:
+    D3D12_GPU_VIRTUAL_ADDRESS gpu_virtual_address_; ///< GPU内存地址
+    void* cpu_virtual_address_; ///< 映射到CPU的内存地址
 };
